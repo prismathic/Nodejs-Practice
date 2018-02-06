8a7784a41855b529a07871c63ffb934dabbd073a
Jesutomiwa <pjjsalam1999@gmail.com>
2018-02-06 06:14:40 +0100

    My first commit!

---
+++ form_test.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Form Test</title>
+</head>
+<body>
+    <form method = "post" action = "validate.php">
+    
+    
+    </form>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+<?php
+//Creating a form with php
+
+?>
